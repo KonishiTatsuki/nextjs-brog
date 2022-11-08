a963bde744da8e7a466ac1b9c17aceef1937a852
konishi <tatsuki.konishi@rakus-partners.co.jp>
2022-11-08 14:59:57 +0900

    ブログ作成

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,20 @@
-import Link from 'next/link';
+import Head from 'next/head';
+import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
       <h1>最初の投稿</h1>
       <h2>
         <Link href="/" legacyBehavior>
           <a>トップページに戻る</a>
         </Link>
       </h2>
+      <Layout>{/* 略 */}</Layout>;
     </>
   );
 }
